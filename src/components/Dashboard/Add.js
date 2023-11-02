e86b6e7d69cdb718438a472c237556186f684273
GideonOdiokine <gidysmartodiokine@gmail.com>
2023-11-02 10:50:28 +0100

    added files

---
+++ src/components/Dashboard/Add.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import Swal from 'sweetalert2';
+
+const Add = ({ employees, setEmployees, setIsAdding }) => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [salary, setSalary] = useState('');
+  const [date, setDate] = useState('');
+
+  const handleAdd = e => {
+    e.preventDefault();
+
+    if (!firstName || !lastName || !email || !salary || !date) {
+      return Swal.fire({
+        icon: 'error',
+        title: 'Error!',
+        text: 'All fields are required.',
+        showConfirmButton: true,
+      });
+    }
+
+    const newEmployee = {
+      firstName,
+      lastName,
+      email,
+      salary,
+      date,
+    };
+
+    employees.push(newEmployee);
+
+    // TODO: Add doc to DB
+
+    setEmployees(employees);
+    setIsAdding(false);
+
+    Swal.fire({
+      icon: 'success',
+      title: 'Added!',
+      text: `${firstName} ${lastName}'s data has been Added.`,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
+
+  return (
+    <div className="small-container">
+      <form onSubmit={handleAdd}>
+        <h1>Add Information</h1>
+        <label htmlFor="firstName">Victim's First Name </label>
+        <input
+          id="firstName"
+          type="text"
+          name="firstName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <label htmlFor="lastName">Victim's Last Name </label>
+        <input
+          id="lastName"
+          type="text"
+          name="lastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <label htmlFor="email">Trafficker' Name</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="salary">Victim's Parent Name </label>
+        <input
+          id="salary"
+          type="number"
+          name="salary"
+          value={salary}
+          onChange={(e) => setSalary(e.target.value)}
+        />
+        <label htmlFor="date">Trafficker's Mobile </label>
+        <input
+          id="date"
+          type="text"
+          name="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+        <div style={{ marginTop: "30px" }}>
+          <input type="submit" value="Add" />
+          <input
+            style={{ marginLeft: "12px" }}
+            className="muted-button"
+            type="button"
+            value="Cancel"
+            onClick={() => setIsAdding(false)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Add;
---
+++ src/components/Dashboard/Edit.js
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import Swal from 'sweetalert2';
+
+const Edit = ({ employees, selectedEmployee, setEmployees, setIsEditing }) => {
+  const id = selectedEmployee.id;
+
+  const [firstName, setFirstName] = useState(selectedEmployee.firstName);
+  const [lastName, setLastName] = useState(selectedEmployee.lastName);
+  const [email, setEmail] = useState(selectedEmployee.email);
+  const [salary, setSalary] = useState(selectedEmployee.salary);
+  const [date, setDate] = useState(selectedEmployee.date);
+
+  const handleUpdate = e => {
+    e.preventDefault();
+
+    if (!firstName || !lastName || !email || !salary || !date) {
+      return Swal.fire({
+        icon: 'error',
+        title: 'Error!',
+        text: 'All fields are required.',
+        showConfirmButton: true,
+      });
+    }
+
+    const employee = {
+      id,
+      firstName,
+      lastName,
+      email,
+      salary,
+      date,
+    };
+
+    // TODO: Update document
+
+    setEmployees(employees);
+    setIsEditing(false);
+
+    Swal.fire({
+      icon: 'success',
+      title: 'Updated!',
+      text: `${employee.firstName} ${employee.lastName}'s data has been updated.`,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
+
+  return (
+    <div className="small-container">
+      <form onSubmit={handleUpdate}>
+        <h1>Edit Record</h1>
+        <label htmlFor="firstName">Victim's First Name</label>
+        <input
+          id="firstName"
+          type="text"
+          name="firstName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <label htmlFor="lastName">Victim's Last Name</label>
+        <input
+          id="lastName"
+          type="text"
+          name="lastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <label htmlFor="email">trafficker' Name</label>
+        <input
+          id="email"
+          type="text"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="salary">Victim's Parent Name</label>
+        <input
+          id="salary"
+          type="number"
+          name="salary"
+          value={salary}
+          onChange={(e) => setSalary(e.target.value)}
+        />
+        <label htmlFor="date">Trafficker's Mobile</label>
+        <input
+          id="date"
+          type="text"
+          name="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+        <div style={{ marginTop: "30px" }}>
+          <input type="submit" value="Update" />
+          <input
+            style={{ marginLeft: "12px" }}
+            className="muted-button"
+            type="button"
+            value="Cancel"
+            onClick={() => setIsEditing(false)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Edit;
